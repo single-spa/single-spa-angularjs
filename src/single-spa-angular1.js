c3b7b6d0421f7cd1ffad7816240868eb24272c13
Joel Denning <joeldenning@gmail.com>
2016-05-09 15:58:41 -0600

    Rewrite for single-spa@2.x

--- src/single-spa-angular1.js
+++ src/single-spa-angular1.js
@@ -1,152 +1,89 @@
-export function defaultAngular1App(config) {
-    if (!config) throw new Error('must provide a config object as the first parameter');
-    if (typeof config.rootElementGetter !== 'function') throw new Error(`must provide a function 'rootElementGetter' that returns the root element to bootstrap`);
-    if (typeof config.publicRoot !== 'string') throw new Error(`must provide a string called 'publicRoot'`);
-    if (typeof config.rootAngularModule !== 'string') throw new Error(`must provide a string called rootAngularModule`);
+let opts;
 
-    let app = {};
-    app.scriptsWillBeLoaded = function() { return scriptsWillBeLoaded.apply(config, arguments) }
-    app.scriptsWereLoaded = function() { return scriptsWereLoaded.apply(config, arguments); }
-    app.applicationWillMount = function() { return applicationWillMount.apply(config, arguments); }
-    app.mountApplication = function() { return mountApplication.apply(config, arguments); }
-    app.applicationWasMounted = function() { return applicationWasMounted.apply(config, arguments); }
-    app.applicationWillUnmount = function() { return applicationWillUnmount.apply(config, arguments); }
-    app.applicationWasUnmounted = function() { return applicationWasUnmounted.apply(config, arguments); }
-    return app;
+const defaultOpts = {
+	// required opts
+	angular: null,
+	domElementGetter: null,
+	mainAngularModule: null,
+
+	// optional opts
+	uiRouter: false,
+};
+
+export default function singleSpaAngular1(userOpts) {
+	if (typeof userOpts !== 'object') {
+		throw new Error(`single-spa-angular1 requires a configuration object`);
+	}
+
+	opts = {
+		...defaultOpts,
+		...userOpts,
+	};
+
+	if (!opts.angular) {
+		throw new Error(`single-spa-angular1 must be passed opts.angular`);
+	}
+
+	if (!opts.domElementGetter) {
+		throw new Error(`single-spa-angular1 must be passed opts.domElementGetter function`);
+	}
+
+	if (!opts.mainAngularModule) {
+		throw new Error(`single-spa-angular1 must be passed opts.mainAngularModule string`);
+	}
+
+	return {
+		bootstrap,
+		mount,
+		unmount,
+	};
 }
 
-export function scriptsWillBeLoaded() {
-    const config = this;
-    return new Promise(function (resolve) {
-        //single-spa owns the base tag, but angular doesn't like that much. This is the workaround.
-        config.nativeGetElementsByTagName = document.getElementsByTagName;
-        document.getElementsByTagName = function(query) {
-            if (query === 'base') {
-                return [];
-            } else {
-                return config.nativeGetElementsByTagName.apply(this, arguments);
-            }
-        }
-        resolve();
-    });
+function bootstrap() {
+	return new Promise((resolve, reject) => {
+		resolve();
+	});
 }
 
-export function scriptsWereLoaded() {
-    var config = this;
+function mount() {
+	return new Promise((resolve, reject) => {
+		window.angular = opts.angular;
 
-    let appAngular;
-    function waitForAngularGlobal(callback) {
-        if (appAngular) {
-            callback(appAngular);
-        } else if (window.angular) {
-            appAngular = window.angular;
-            callback(appAngular);
-        } else {
-            setTimeout(function() {
-                waitForAngularGlobal(callback);
-            }, 3);
-        }
-    }
-    config.angularPromise = function() {
-        return new Promise(function (resolve) {
-            waitForAngularGlobal((angular) => resolve(angular));
-        })
-    }
+		const containerEl = getContainerEl();
+		const bootstrapEl = document.createElement('div');
+		containerEl.appendChild(bootstrapEl);
 
-    return new Promise(function(resolve) {
-        config.angularPromise()
-        .then(function(angular) {
-            config.jQuery = window.jQuery;
-            angular.module(config.rootAngularModule).factory('SingleSpaPrefixURLsInterceptor', function() {
-                return {
-                    request: function(requestConfig) {
-                        requestConfig.url = window.singlespa.prependUrl(config.publicRoot, requestConfig.url);
-                        return requestConfig;
-                    }
-                };
-            });
+		if (opts.uiRouter) {
+			const uiViewEl = document.createElement('div');
+			uiViewEl.setAttribute('ui-view', '');
+			bootstrapEl.appendChild(uiViewEl);
+		}
 
-            angular.module(config.rootAngularModule).config(function($httpProvider, $locationProvider) {
-                $httpProvider.interceptors.push('SingleSpaPrefixURLsInterceptor');
-            });
-            resolve();
-        })
-        .catch((ex) => {
-            throw ex;
-        });
-    });
+		opts.angular.bootstrap(bootstrapEl, [opts.mainAngularModule])
+
+		resolve();
+	});
 }
 
-export function applicationWillMount() {
-    const config = this;
-    return new Promise(function (resolve) {
-        window.jQuery = config.jQuery;
-        document.getElementsByTagName = function(query) {
-            if (query === 'base') {
-                return [];
-            } else {
-                return config.nativeGetElementsByTagName.apply(this, arguments);
-            }
-        }
-        resolve();
-    });
+function unmount() {
+	return new Promise((resolve, reject) => {
+		let rootScope = angular.injector(['ng']).get('$rootScope');
+		const result = rootScope.$destroy();
+
+		getContainerEl().innerHTML = '';
+
+		if (opts.angular === window.angular)
+			delete window.angular;
+
+		setTimeout(resolve);
+	});
 }
 
-export function applicationWasMounted() {
-    const config = this;
-    if (config.numMounts) {
-        config.numMounts++;
-    } else {
-        config.numMounts = 1;
-    }
-    return new Promise(function (resolve) {
-        config.angularPromise()
-        .then((angular) => {
-            //leak global
-            window.angular = angular;
+function getContainerEl() {
+	const element = opts.domElementGetter();
+	if (!element) {
+		throw new Error(`domElementGetter did not return a valid dom element`);
+	}
 
-            //bootstrap
-            let element = config.rootElementGetter()
-            let numMountsAutoMounted = document.querySelector('[ng-app]') ? 1 : 0;
-            if (config.numMounts > numMountsAutoMounted) {
-                //we need to bootstrap
-                angular.bootstrap(element, [config.rootAngularModule]);
-            } else if (document.documentElement.getAttribute('ng-app')) {
-                angular.bootstrap(element, [document.documentElement.getAttribute('ng-app')]);
-            }
-            resolve()
-        })
-        .catch((ex) => {
-            throw ex;
-        });
-    });
-}
-
-export function applicationWillUnmount() {
-    const config = this;
-    return new Promise(function (resolve) {
-        config.angularPromise()
-        .then((angular) => {
-            let rootScope = angular.injector(['ng']).get('$rootScope');
-            rootScope.$destroy();
-            delete window.angular;
-            delete window.jQuery;
-            resolve()
-        })
-        .catch((ex) => {
-            throw ex;
-        });
-    })
-}
-
-export function applicationWasUnmounted() {
-    const config = this;
-    return new Promise(function (resolve) {
-        document.getElementsByTagName = config.nativeGetElementsByTagName;
-        resolve();
-    })
-}
-
-function removeRedundantSlashes(str) {
-    return str.replace(/[\/]+/g, '/');
+	return element;
 }
